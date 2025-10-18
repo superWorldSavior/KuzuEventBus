81defb79349830d50d7db7607863e98d657ab509
superWorldSavior <erpesle@gmail.com>
2025-10-18 04:09:58 +0000

    chore: remove deprecated lead analysis feature and components (strange, was from casys seo ?)

--- apps/casys-website/src/features/lead/components/opportunities-analysis/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as KeywordClustersTable } from './KeywordClustersTable.qwik';
-export { default as OpportunitiesHeader } from './OpportunitiesHeader.qwik';
-export { default as QuickWinsSection } from './QuickWinsSection.qwik';
-export { default as SuggestedTopicsGrid } from './SuggestedTopicsGrid.qwik';
